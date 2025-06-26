9c624b8fb6dadf6bc5ae78c5810556a9dd142e90
Striver11-Praveen Kumar <83543044+striver11@users.noreply.github.com>
2025-06-26 20:12:53 +0000

    Create Comparison_Operator.js

---
+++ Comparison_Operator.js
@@ -0,0 +1,47 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+console.log("Try programiz.pro");
+
+
+//string comparision is done by Lexicographical comparison
+//A=65 (+32)=a    //accsi value comparision
+console.log("apple"<'banana')
+
+//comparison of the numbers with out considiring their data types
+console.log("01"==01)
+
+//comparision of the by considering the data types too
+//Strict equality  (ctrl +/)- single line comment
+// /*String equaility*/ (ctrl +shift+/)-multiline comment
+console.log("01"===01)
+
+
+console.log(undefined==null,`<-undefined == null`)
+console.log(undefined === null,`<-undefined ===null`)
+
+//comparing null with other values
+//with >,<,<=,>= the value of null is 0 please make sure when you are doing the a number comparing please do a null check before other wise if the number is null the below things might happen
+console.log("null >0 =>", null >0)
+console.log("null >1 =>", null>1)
+console.log("null <0 =>",null <0)
+console.log("null <1 =>", null <1)
+console.log("null <=1 =>", null <=1)
+console.log("null >=0 =>", null >=0)//
+console.log("null <=0 =>", null <=0)
+
+// with == the value of null not 0
+console.log("null==0 =>",null==0);
+
+
+//comparing undefined with other values
+//it is always false
+console.log("undefined >0 =>", undefined >0)
+console.log("undefined >1 =>", undefined>1)
+console.log("undefined <0 =>",undefined <0)
+console.log("undefined <1 =>", undefined <1)
+console.log("undefined <=1 =>", undefined <=1)
+console.log("undefined >=0 =>", undefined >=0)//
+console.log("undefined <=0 =>", undefined <=0)
+console.log("undefined==0 =>",undefined==0);//observe this too
+
